459ca0e2119cb5594ca6d9bba694de412c3e36bb
Andrius <andriuss0316@gmail.com>
2021-08-12 18:07:06 +0300

    20210812

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
-// astuoniolikta c uzduotis
+// astuoniolikta d uzduotis
 
 
 
+// PAPILDOMI KOMENTARAI:
+// Iprastinio if panaudojimas pasirenkant langelius
+
+if (r % 2 === 0) {
+  cellHTML = whiteBlackCellHTML;
+} else {
+  cellHTML = blackWhiteCellHTML;
+}
+// Ternary operatoriaus panaudojimas pasirenkant langelius
+
+cellHTML = r % 2 === 0 ? whiteBlackCellHTML : blackWhiteCellHTML;
